d5ded5f622fda0f39ebb719028288634b32c924b
Thịnh Trần <thinhtran3803@gmail.com>
2023-04-23 17:17:26 +0700

    add datamanager

---
+++ StudentManagement/StudentManagement/Helper/DataManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using DocumentFormat.OpenXml.Wordprocessing;
+using StudentManagement.Models;
+using StudentManagement.Utils;
+
+
+namespace StudentManagement.Helper {
+    public class DataManager {
+        private static List<Account> adminAccounts = new List<Account>();
+        private SqlDataReader _dataReader;
+
+        private static DataManager instance = null;
+
+        private DataManager() {
+            initAccount();
+        }
+        
+        private void initAccount() {
+            _dataReader = ExecuteQuery.executeReader("Select * from AdminAccount");
+            while (_dataReader.Read()) {
+                string username = _dataReader.GetString(_dataReader.GetOrdinal("username"));
+                string password = _dataReader.GetString(_dataReader.GetOrdinal("password"));
+                Account account = new Account(username, password);
+                adminAccounts.Add(account);
+            }
+        }
+
+        public static List<Account> getAdminAccounts() {
+            if(instance == null) {
+                instance = new DataManager();
+            }
+            return adminAccounts;
+        }
+
+    }
+
+}
