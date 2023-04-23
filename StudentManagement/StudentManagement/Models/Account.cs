528c1075a7d2e6429534be6d5ee8abfc2587a85b
Thịnh Trần <thinhtran3803@gmail.com>
2023-04-23 17:17:12 +0700

    add class account

---
+++ StudentManagement/StudentManagement/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace StudentManagement.Models {
+    public class Account {
+        private string username;
+        private string password;
+
+        public Account(string username, string password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string getUsername()
+        {
+            return username;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+
+
+    }
+}
