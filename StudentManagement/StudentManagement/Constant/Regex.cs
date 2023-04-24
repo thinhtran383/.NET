eab9a8771b7445f30d29ee8c4b91e603ac8aca9f
Thịnh Trần <thinhtran3803@gmail.com>
2023-04-24 09:45:46 +0700

    add regex check email

---
+++ StudentManagement/StudentManagement/Constant/Regex.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Constant {
+    internal class Regex {
+        public static string EMAIL = "^[\\w-\\.]+@st\\.phenikaa-uni\\.edu\\.vn$";
+        public static String PHONE = "^[0-9]{10}$";
+    }
+}
