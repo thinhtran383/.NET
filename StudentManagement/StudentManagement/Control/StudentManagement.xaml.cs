d82e2ce5b8642eeca150ad6c4e28619e2d24d721
Thịnh Trần <thinhtran3803@gmail.com>
2023-04-30 13:27:59 +0700

    done btn Delete, Export

--- StudentManagement/StudentManagement/Control/StudentManagement.xaml.cs
+++ StudentManagement/StudentManagement/Control/StudentManagement.xaml.cs
@@ -21,7 +21,7 @@ using StudentManagement.Utils;
 
 namespace StudentManagement.Control {
 
-    public partial class StudentManagement : UserControl {
+    public partial class StudentManagement:UserControl {
         private ObservableCollection<Student> studentList = DataManager.getStudentList();
 
         public StudentManagement() {
@@ -29,41 +29,15 @@ namespace StudentManagement.Control {
             initNganh();
         }
 
-        private void dgStudent_Loaded(object sender, RoutedEventArgs e) {
+        private void dgStudent_Loaded(object sender,RoutedEventArgs e) {
             dgStudent.ItemsSource = studentList;
         }
 
-        private void dgStudent_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            if (dgStudent.SelectedItem != null) {
-                // Lấy đối tượng Student được chọn
-                dynamic selectedStudent = dgStudent.SelectedItem;
-
-                string maSv = selectedStudent["Mã sinh viên"].ToString();
-                string maNganh = selectedStudent["Mã ngành"].ToString();
-                string hoTen = selectedStudent["Họ tên"].ToString();
-                string ngaySinh = selectedStudent["Ngày sinh"].ToString("dd/MM/yyyy");
-                string gioiTinh = selectedStudent["Giới tính"].ToString();
-                string soDT = selectedStudent["Số điện thoại"].ToString();
-                string email = selectedStudent["Email"].ToString();
-                string khoa = selectedStudent["Khoa"].ToString();
-
-                txtMaSinhVien.Text = maSv; 
-                cbNganh.Text = maNganh;
-               
-                txtTenSinhVien.Text = hoTen;
-                txtDienThoai.Text = soDT;
-                txtEmail.Text = email;
-                txtKhoa.Text = khoa;
-                cbGioiTinh.Text = gioiTinh;
-                pickNgaySinh.Text = ngaySinh;
-            }
-
-
-        }
+      
 
         private void clear() {
             txtMaSinhVien.Text = "";
-        
+
             cbNganh.Text = "";
             txtTenSinhVien.Text = "";
             txtDienThoai.Text = "";
@@ -77,28 +51,18 @@ namespace StudentManagement.Control {
 
         private void initNganh() {
             SqlDataReader dataReader = ExecuteQuery.executeReader("select MaNganh from Nganh");
-            while (dataReader.Read()) {
+            while(dataReader.Read()) {
                 cbNganh.Items.Add(dataReader["MaNganh"].ToString());
             }
         }
 
         private void btnAdd_Click(object sender,RoutedEventArgs e) {
-            DateTime date = DateTime.Parse(pickNgaySinh.Text);
-            string sqlAdd = "INSERT INTO SinhVien VALUES ('" + txtMaSinhVien.Text + "', '" + cbNganh.Text + "', N'" + txtTenSinhVien.Text + "', '" + date + "', N'" + cbGioiTinh.Text + "', '" + txtDienThoai.Text + "', '" + txtEmail.Text + "', N'" + txtKhoa.Text + "')";
-           // ExecuteQuery.executeNonQuery(sqlAdd);
-          
-
-            // Chuyển đổi giá trị của pickNgaySinh.Text sang kiểu DateTime
-          
-
-            // Tạo một đối tượng Student mới và thêm vào danh sách studentList
-            studentList.Add(new Student(txtMaSinhVien.Text,cbNganh.Text,txtTenSinhVien.Text,date,cbGioiTinh.Text,txtDienThoai.Text,txtEmail.Text,txtKhoa.Text));
-
-            // Cập nhật lại hiển thị danh sách sinh viên
-            dgStudent.Items.Refresh();
-
-            // Xóa dữ liệu đã nhập trong các trường
-            clear();
+            //DateTime date = DateTime.Parse(pickNgaySinh.Text);
+            //string sqlAdd = "insert into SinhVien values('" + txtMaSinhVien.Text + "','" + cbNganh.Text + "',N'" + txtTenSinhVien.Text + "','" + date + "',N'" + cbGioiTinh.Text + "','" + txtDienThoai.Text + "','" + txtEmail.Text + "',N'" + txtKhoa.Text + "')";
+            //ExecuteQuery.executeNonQuery(sqlAdd);
+            //studentList.Add(new Student(txtMaSinhVien.Text,cbNganh.Text,txtTenSinhVien.Text,date,cbGioiTinh.Text,txtDienThoai.Text,txtEmail.Text,txtKhoa.Text));
+            //dgStudent.Items.Refresh();
+            //clear();
         }
 
 
@@ -107,7 +71,13 @@ namespace StudentManagement.Control {
         }
 
         private void btnDelete_Click(object sender,RoutedEventArgs e) {
-
+            if(MessageBox.Show("Bạn có chắc muốn xoá?","Xác nhận xoá",MessageBoxButton.YesNo) == MessageBoxResult.Yes) {
+                string selectedId = ((Student) dgStudent.SelectedItem).MaSV;
+                string sqlDelete = "delete from SinhVien where MaSinhVien = '" + selectedId + "'";
+                ExecuteQuery.executeNonQuery(sqlDelete);
+                studentList.Remove((Student) dgStudent.SelectedItem);
+                dgStudent.Items.Refresh();
+            }
         }
 
         private void btnClear_Click(object sender,RoutedEventArgs e) {
@@ -115,9 +85,9 @@ namespace StudentManagement.Control {
         }
 
         private void btnExport_Click(object sender,RoutedEventArgs e) {
-
+            ExcelExporter.Export(dgStudent,"studentList.xlsx");
         }
 
-       
+
     }
 }
