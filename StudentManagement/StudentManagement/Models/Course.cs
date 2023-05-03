84a3d6550e6233dc633f86e0ee7c3dfe91e536dd
Thịnh Trần <thinhtran3803@gmail.com>
2023-05-03 17:53:26 +0700

    add coures model

---
+++ StudentManagement/StudentManagement/Models/Course.cs
@@ -0,0 +1,31 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models {
+    public class Course {
+        private string maMonHoc;
+        private string tenMonHoc;
+        private int soTinChi;
+
+        public Course(string maMonHoc, string tenMonHoc, int soTinChi) {
+            this.maMonHoc = maMonHoc;
+            this.tenMonHoc = tenMonHoc;
+            this.soTinChi = soTinChi;
+        }
+        public int SoTinChi {
+            get => soTinChi;
+            set => soTinChi = value;
+        }
+        public string TenMonHoc {
+            get => tenMonHoc;
+            set => tenMonHoc = value;
+        }
+        public string MaMonHoc {
+            get => maMonHoc;
+            set => maMonHoc = value;
+        }
+    }
+}
