6485a0144bd1b8e45740421593e0f6cc8fe75000
Thịnh Trần <thinhtran3803@gmail.com>
2023-05-03 17:54:44 +0700

    load data on datagrid

--- StudentManagement/StudentManagement/Control/CourseManagement.xaml.cs
+++ StudentManagement/StudentManagement/Control/CourseManagement.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using StudentManagement.Helper;
+using StudentManagement.Models;
 
 namespace StudentManagement.Control
 {
-    public partial class CourseManagement: UserControl
-    {
+    public partial class CourseManagement: UserControl {
+        private ObservableCollection<Course> coursesList = DataManager.GetCourseList();
         public CourseManagement()
         {
             InitializeComponent();
+        }
+
+        private void dgCourses_Loaded(object sender,RoutedEventArgs e) {
+            dgCourses.ItemsSource = coursesList;
         }
     }
 }
