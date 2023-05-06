4e468aa9d7ebbbfe7890314d3fd00868c941f174
Thịnh Trần <thinhtran3803@gmail.com>
2023-05-06 17:10:49 +0700

    add credit regex

--- StudentManagement/StudentManagement/Constant/Regex.cs
+++ StudentManagement/StudentManagement/Constant/Regex.cs
@@ -10,6 +10,7 @@ namespace StudentManagement.Constant {
     internal class Regex {
         public static string EMAIL = "^[\\w-\\.]+@st\\.phenikaa-uni\\.edu\\.vn$";
         public static string PHONE = "^[0-9]{10}$";
-        public static string CREDITS = "^\\d+$";
+        public static string CREDITS = "^[1-9]\\d*$";
+    
     }
 }
