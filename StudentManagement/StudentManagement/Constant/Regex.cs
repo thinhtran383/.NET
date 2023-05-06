137d0502566954e9ae6c156bfbeb17711fef88b7
Thịnh Trần <thinhtran3803@gmail.com>
2023-05-06 09:20:19 +0700

    add properties CREDITS

--- StudentManagement/StudentManagement/Constant/Regex.cs
+++ StudentManagement/StudentManagement/Constant/Regex.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 namespace StudentManagement.Constant {
     internal class Regex {
         public static string EMAIL = "^[\\w-\\.]+@st\\.phenikaa-uni\\.edu\\.vn$";
-        public static String PHONE = "^[0-9]{10}$";
+        public static string PHONE = "^[0-9]{10}$";
+        public static string CREDITS = "^\\d+$";
     }
 }
